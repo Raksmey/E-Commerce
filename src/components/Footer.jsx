74b85130aeaec518bb0b98ee9c4bbf387038ae9f
Raksmey <raksmeyngoy0@gmail.com>
2025-09-15 20:50:26 +0700

    intit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-
+import { NavLink } from "react-router-dom"
 
 function Footer() {
     return (
@@ -8,13 +8,13 @@ function Footer() {
                     <div className="flex flex-col lg:flex-row justify-between items-center  max-w-[90%] lg:max-w-5xl mx-auto">
                         <h1>Copyright &#169; -MasterIT Store</h1>
                         <ul className="flex items-center gap-4">
-                            <li><a href="">Home</a></li>
-                            <li><a href="">Contact Us</a></li>
-                            <li><a href="">About Us</a></li>
-                            <li><a href="">Term and conditional</a></li>
+                            <li><NavLink to="/">Home</NavLink></li>
+                            <li><NavLink to="/products">Contact Us</NavLink></li>
+                            <li><NavLink to="/contact">Term and conditional</NavLink></li>
+                            <li><NavLink to="/about">About Us</NavLink></li>
                         </ul>
-                    </div>
 
+                    </div>
                 </div>
             </footer>
         </>
